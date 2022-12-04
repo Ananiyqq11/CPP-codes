7f7927b6ecc5364100f001e926e78de7cd673954
Ananiya <ananiya123416@gmail.com>
2022-12-04 16:01:22 +0300

    for and if statement

---
+++ %5 Beep.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{ int n;
+  cout<<"enter number"<<endl;
+  cin>>n;
+  for(int i=0;i=n;i++){
+    cout <<n--<<endl;
+     if(i%5==0)   
+    cout<<"Beep"<<endl;      
+            }
+    return 0;
+}
